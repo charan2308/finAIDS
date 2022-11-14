6b6e246f2ddf8077c2af38f9f0c3bbb14f1155ba
DivyanshVinayak23 <divyansh.vinayak@gmail.com>
2022-11-14 11:49:42 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 
 import './App.css';
 
 
 const App = () => (
-  <div class="container py-4">
+<div>
+<Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">finAIDS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+<div class="container py-4">
     <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
       <h1 class="display-5 fw-bold">finAIDS</h1>
@@ -12,6 +39,7 @@ const App = () => (
       <button class="btn btn-primary btn-lg" type="button">Example button</button>
     </div>
     </div>
+</div>
 </div>
 );
 

ff3774430b7054b5bc941fe7117599ddfcf9181a
DivyanshVinayak23 <divyansh.vinayak@gmail.com>
2022-11-14 11:36:43 +0530

    Added Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
+
+
 import './App.css';
-import React from 'react'
-import ThemeSwitcher from "./ThemeSwitcher";
 
 
-function App() {
-
-  return (
-    <div className="App">
-      <div className="App min-vh-100 d-flex justify-content-center align-items-center">
-      <div>
-        <ThemeSwitcher/>
-      </div>
-      </div>
+const App = () => (
+  <div class="container py-4">
+    <div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">finAIDS</h1>
+      <p class="col-md-8 fs-4">Your all in one Financial Advisior</p>
+      <button class="btn btn-primary btn-lg" type="button">Example button</button>
     </div>
-  );
-}
+    </div>
+</div>
+);
 
 export default App;

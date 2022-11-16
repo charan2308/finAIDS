401e4db26bd28c3e01d47f5af49c03f1f6af5674
DivyanshVinayak23 <divyansh.vinayak@gmail.com>
2022-11-16 12:36:35 +0530

    Added Brand Image

--- src/App.js
+++ src/App.js
@@ -2,35 +2,27 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import logo from './logo3.svg'
 
 import './App.css';
 
 
 const App = () => (
 <div>
-<Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">finAIDS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+<Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt="logo"
+              src={logo}
+              width="40"
+              height="40"
+              className="d-inline-block align-top"
+            />{' '}
+            finAIDS
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
 <div class="container py-4">
     <div class="p-5 mb-4 bg-light rounded-3">
     <div class="container-fluid py-5">
